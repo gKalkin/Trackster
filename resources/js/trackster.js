3565a68c412f8e98b683df1cf63d4407024f1a2f
unknown <myers.gmm@gmail.com>
2018-07-07 19:23:31 -0700

    track listings response

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,11 +1,32 @@
 var Trackster = {};
 
+const API_Key = "b1065b2c652b55899ce83bc24f3943b2"
+
+$(document).ready(function() {
+  $('#search-btn').click(function() {
+    Trackster.searchTracksByTitle($("#search-input").val());
+  });
+});
+
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  for(var index = 0; index < tracks.length; index++) {
+    console.log(tracks[index]);
+    var track_html = '<div class="row track">' +
+      '<div class="col-xs-1 col-xs-offset-1 play-btn">' +
+      '<a href="#"><i class="fa fa-play-circle-o fa-2x"></i></a></div>' +
+      '<div class="col-xs-2 track-info">' + tracks[index].name + '</div>' +
+      '<div class="col-xs-2 track-info">Test</div><div class="col-xs-2 track-info">Test</div>' +
+      '<div class="col-xs-2 track-info">' +
+      '<img src="https://images-na.ssl-images-amazon.com/images/I/61wPDPwiBSL._SS500.jpg" alt="" ' +
+      'height="64" width="64"></div>' +
+      '<div class="col-xs-2 track-info">Test</div></div>';
 
+      $('#track-list').append(track_html)
+    }
 };
 
 /*
@@ -13,5 +34,10 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_Key + "&format=json",
+    success: function(response) {
+      Trackster.renderTracks(response.results.trackmatches.track);
+    }
+  })
 };
